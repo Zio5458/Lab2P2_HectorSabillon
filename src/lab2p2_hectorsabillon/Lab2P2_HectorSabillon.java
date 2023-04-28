fa9400a07c3f33fb04faff91077f3990524decfd
Hector Sabillon <hdannysabillon992232@gmail.com>
2023-04-28 13:27:36 -0600

    uno

---
+++ src/lab2p2_hectorsabillon/Lab2P2_HectorSabillon.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p2_hectorsabillon;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Lab2P2_HectorSabillon {
+
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList pinturas = new ArrayList();
+    static ArrayList esculturas = new ArrayList();
+    static ArrayList fotografias = new ArrayList();
+    static ArrayList escritos = new ArrayList();
+
+    public static void main(String[] args) {
+
+    }
+
+}
